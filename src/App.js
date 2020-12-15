29297112402493663613dbf45ab7785df3a41fd5
Pedram Valiani <p.valiani@gmail.com>
2020-12-15 15:41:07 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
+
+
 import './App.css';
 
 function App() {
+
+// defines a destructured array of inputNumber with a method to control the state. Initial state of inputNumber is 0
+  const [inputNumber, setNumber] = useState(0);
+  const [answer, setAnswer] = useState("");
+
+
+// our fizzbuzz useEffect arrow function - inputNumber as second argument as we are "watching" it
+
+  useEffect(()=>{
+
+    if (inputNumber % 3 === 0 && inputNumber % 5 === 0) {
+      setAnswer("fizzbuzz")
+    } else if (inputNumber % 3 === 0) {
+      setAnswer("fizz")
+    } else if (inputNumber % 5 === 0) {
+      setAnswer("buzz")
+    } else {
+      setAnswer(inputNumber);
+    }
+
+
+  },[inputNumber])
+
+// handles the increment of inputNumber for fizzbuzz 
+  const handleInc = () => {
+    setNumber(inputNumber + 1);
+
+  }
+
+
+// returns a button which can be clicked and displays the current inputNumber
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={handleInc}> + </button>
+      {/* show answer as it will vary between number/string as the increment goes on */}
+      <h1>{answer}</h1>
+    </>
   );
 }
 
